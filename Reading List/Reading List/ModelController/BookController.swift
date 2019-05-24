32ac9a1180883f886192240f4094b7319fe0fe57
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-24 11:37:43 -0700

    Created Book swift file and BookController

---
+++ Reading List/Reading List/ModelController/BookController.swift
@@ -0,0 +1,14 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Enayatullah Naseri on 5/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+    
+}
