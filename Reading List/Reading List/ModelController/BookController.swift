a3cc8d6742c4391a7ca837f8ad539873b791987a
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-24 18:52:51 -0700

    BookController updated

--- Reading List/Reading List/ModelController/BookController.swift
+++ Reading List/Reading List/ModelController/BookController.swift
@@ -9,6 +9,53 @@
 import Foundation
 
 class BookController {
-    var books: [Book] = []
+    private(set) var books: [Book] = []
     
+    
+    var  readingListURL: URL? {
+        
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return documents.appendingPathComponent("readingList.plist")
+    }
+    
+    func saveToPersistentStore() {
+        guard let url = readingListURL else {
+            return
+        }
+        do {
+            
+            let encoder = PropertyListEncoder()
+            let booksData = try encoder.encode(books)
+            try booksData.write(to: url)
+            
+        } catch {
+            NSLog("Error saving to disk: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = readingListURL, fileManager.fileExists(atPath: url.path) else {
+            return
+        }
+        do {
+            
+            let booksData = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: booksData)
+            
+        } catch {
+            NSLog("Error loading from disk: \(error)")
+        }
+    }
+    
+    func updateHasBeenRead(for book: Book) {
+        guard let index = books.index(of: book) else { return }
+        books[index].hasBeenRead = !books[index].hasBeenRead
+        //
+        //
+    }
 }
